9459fa3beb110711a235b0e9bd34585363c9fbdc
Your Name <108307350+pjduri@users.noreply.github.com>
2023-10-23 01:09:08 -0500

    console practice and testing repos

--- hello.js
+++ hello.js
@@ -4,8 +4,8 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
-
+console.log("Hello World!", "Pizza time!", "Happy Halloween!")
+console.log('Quoth the raven, "Nevermore!"')
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
